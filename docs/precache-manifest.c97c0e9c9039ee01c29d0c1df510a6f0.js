fd861df14895230058d08c02515d47a41a9d693c
Cris Martin <cristina.martin@cabify.com>
2021-01-28 18:58:41 +0100

    Update docs

---
+++ docs/precache-manifest.c97c0e9c9039ee01c29d0c1df510a6f0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcec922d302ed1874afb2eabdfc2bf40",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ad7d8ff2bb279ac19528",
+    "url": "./static/css/main.e31cb040.chunk.css"
+  },
+  {
+    "revision": "b382a68e7f08fc2ab238",
+    "url": "./static/js/2.b9c8bd3c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.b9c8bd3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad7d8ff2bb279ac19528",
+    "url": "./static/js/main.2b4cf0d8.chunk.js"
+  },
+  {
+    "revision": "b7830c4bddc1e33e0273",
+    "url": "./static/js/runtime-main.09816790.js"
+  },
+  {
+    "revision": "2104fd92691711423a56bc7f9584b4e9",
+    "url": "./static/media/fondo.2104fd92.jpg"
+  },
+  {
+    "revision": "c8aab8bd4cd12882428b39104fe7965f",
+    "url": "./static/media/logo.c8aab8bd.png"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6a8da8514c39c1723c6f8242388d1eea.js"
+  "./precache-manifest.c97c0e9c9039ee01c29d0c1df510a6f0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
